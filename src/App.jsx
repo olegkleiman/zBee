99324c072e96a33b2132711b567206707ea25277
Oleg Kleiman <oleg_kleyman@hotmail.com>
2019-04-04 23:34:22 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 // @flow
 import React from 'react';
 import DataVis from './DataVis';
+import { DataProvider} from './DataContext';
+
+type State = {
+  dataUrl: string,
+  configUrl: string
+};
 
 export default
-class App extends React.Component<Props, State> {
+class App extends React.Component<State, {}>  {
+
+  state = {
+    dataUrl: 'https://zbeegqlserver.herokuapp.com/data/commutes.csv',
+    configUrl: 'https://zbeegqlserver.herokuapp.com/data/kepler.commutes.config.json',
+    onDataUrlChanged: ::this.onDataUrlUpdated
+  }
+
+  onDataUrlUpdated(url: String) {
+    this.setState({
+      dataUrl: url
+    });
+  }
 
   render() {
-    return (
-      <DataVis />
-    )
+    return (<DataProvider value={this.state}>
+              <DataVis dataUrl={this.state.dataUrl}
+                       configUrl={this.state.configUrl} />
+            </DataProvider>)
   }
 }
